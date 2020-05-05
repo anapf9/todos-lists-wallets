dc7829e4df3f918b7bc96dd2aeb72e5d6fcad83b
Ana <ana.eng.dev@gmail.com>
2020-05-05 18:27:07 -0300

    getters como metodo que retorna uma tarefa por id

--- src/store.js
+++ src/store.js
@@ -16,11 +16,21 @@ export default new Vuex.Store ({
     // funcionam como as compiuted propreties dos components, pois fazem um cache do resultadp retornado.
     // o getter sera reexecutado quando a propriedade tarefas for alterada, como as computed propreties
     getters: {
+        // forma completa
         // tarefasConcluidas: (state) => {
         //     return state.tarefas.filter(t => t.concluido)
         // },
         tarefasConcluidas: state => state.tarefas.filter(t => t.concluido),
         tarefasAFazer: state => state.tarefas.filter(t => !t.concluido),
-        totalDeTarefasConcluidas: (state, getters) => getters.tarefasConcluidas.length
+        totalDeTarefasConcluidas: (state, getters) => getters.tarefasConcluidas.length,
+        // uma função dentro de outra função é uma closure
+        // forma completa
+        // buscarTarefaPorId: (state, getters) => {
+        //     return (id) => {
+        //         return state.tarefas.find(t => t.id === id)
+        //     }
+        // }
+        buscarTarefaPorId: state => id => state.tarefas.find(t => t.id === id)
+        
     }
 })
